71b4001d244f4734cc0d1e030784dc97dec7e803
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-12 17:49:01 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,13 +7,12 @@ def second_challenge
       :hero => {},
       :hero_friends => []
     },
-
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }
-    }
+  }
+}
 end
