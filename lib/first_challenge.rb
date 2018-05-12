2367fb26aa6d6890d1cd9fde739483901567243e
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-12 17:44:36 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,11 @@
 def first_challenge
+  # Set the epic_tragedy variable equal to a hash with two key/value pairs.
   epic_tragedy = {
+    # The keys should be symbols of the family names (:montague and :capulet).
+    # The values should be empty hashes.
     :montague => {},
     :capulet => {}
 }
 end
+
+# and the values should be empty hashes
